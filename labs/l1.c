a4eae994dade1a8b0a94cb53a0475b41097fbbbd
GalaxyShad <vadim.kadyrin@mail.ru>
2022-09-04 19:30:15 +0500

    refactor: main func

--- labs/l1.c
+++ labs/l1.c
@@ -73,38 +73,35 @@ void fill_arr_sin(key_t* arr, size_t n, key_t min, key_t max, int r) {
 
 
 void fill_arr_unk(key_t* arr, size_t n, key_t min, key_t max, int r) {
-	if (!arr) return;
-
-	int delta = max - min;
-
-	for (int i = 0; i < n; ++i) {
-		arr[i] = (min + delta * ((double)i / n)) % max;
-	}
+	// TODO
 }
 
 
-#define n 30
+#define N 30
 
 int main() {
 	srand(time(NULL));
 
-	
-	key_t arr[n] = {0};
+	key_t arr[N] = {0};
+	struct {
+		key_t start;
+		key_t end;
+	} interval = {2, 6};
 
-	fill_arr_random(arr, n, 2, 12, 0);
-	print_arr_int((const int*)arr, n);
+	fill_arr_random(arr, N, interval.start, interval.end, 0);
+	print_arr_int((const int*)arr, N);
 
-	fill_arr_linear_upwards(arr, n, 3, 12, 0);
-	print_arr_int((const int*)arr, n);
+	fill_arr_linear_upwards(arr, N, interval.start, interval.end, 0);
+	print_arr_int((const int*)arr, N);
 
-	fill_arr_linear_downwards(arr, n, 3, 12, 0);
-	print_arr_int((const int*)arr, n);
+	fill_arr_linear_downwards(arr, N, 2, 3, 0);
+	print_arr_int((const int*)arr, N);
 
-	fill_arr_sin(arr, n, 3, 12, 0);
-	print_arr_int((const int*)arr, n);
+	fill_arr_sin(arr, N, 3, 12, 0);
+	print_arr_int((const int*)arr, N);
 
-	fill_arr_unk(arr, n, 3, 12, 0);
-	print_arr_int((const int*)arr, n);
+	fill_arr_unk(arr, N, 3, 12, 0);
+	print_arr_int((const int*)arr, N);
 
 	return 0;
 }
