6820ed2b10e34f7e19aab2fd2bbb2c9de66f41ac
GalaxyShad <vadim.kadyrin@mail.ru>
2022-10-02 21:56:26 +0500

    hz

--- labs/l2.c
+++ labs/l2.c
@@ -39,33 +39,99 @@ typedef struct sortFuncCell {
     size_t      rangeStep;
 } sortFuncCell_t;
 
-
-
-
-// utils
-int 	random_int(int min, int max);
-double 	random_double(double min, double max);
-
-// buisness logic
-void 	fill_arr_random(key_t* arr, size_t n, key_t min, key_t max, int r);
-void 	fill_arr_linear_upwards(key_t* arr, size_t n, key_t min, key_t max, int r);
-void 	fill_arr_linear_downwards(key_t* arr, size_t n, key_t min, key_t max, int r);
-void 	fill_arr_sin(key_t* arr, size_t n, key_t min, key_t max, int r);
-void 	fill_arr_sawtooth(key_t* arr, size_t n, key_t min, key_t max, int r);
-void 	fill_arr_stepped(key_t* arr, size_t n, key_t min, key_t max, int r);
-void 	fill_arr_quasi_ordered(key_t* arr, size_t n, key_t min, key_t max, int r);
-
-
-
+///////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////
 
+
+DWORD     get_tick_count()   { return GetTickCount(); }
+
+
 static uint64_t s_compCount = 0;
+uint64_t  get_comp_count()   { return s_compCount;    }
+void      reset_comp_count() { s_compCount = 0;       }
 
-DWORD get_tick_count()   { return GetTickCount(); }
 
-uint64_t get_comp_count()   { return s_compCount;    }
-void     reset_comp_count() { s_compCount = 0;       }
+int     random_int(int min, int max) { return rand() % (max - min) + min; }
+double  random_double(double min, double max) { return ((double)rand() / RAND_MAX) * (max - min) + min; }
 
+
+void print_arr(const key_t* arr, size_t n) {
+    for (int i = 0; i < n; i++) printf("%"MODIFIER" ", arr[i]);
+    printf("\n");
+}
+
+void printf_ex(FILE* file, const char* fmt, ...) {
+    va_list ap;
+
+    va_start(ap, fmt);
+    vprintf(fmt, ap);
+    va_end(ap);
+
+    va_start(ap, fmt);
+    vfprintf(file, fmt, ap);
+    va_end(ap);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+void fill_arr_random(key_t * arr, size_t n, key_t min, key_t max, int r) {
+	if (!arr) return;
+
+	for (int i = 0; i < n; ++i) {
+		arr[i] = (key_t)random_double(min, max); 
+	}
+}
+
+
+void fill_arr_linear_upwards(key_t * arr, size_t n, key_t min, key_t max, int r) {
+	if (!arr) return;
+
+	key_t delta = (max - min);
+
+	for (int i = 0; i < n; ++i) {
+		arr[i] = delta * (i / (double)(n-1)) + min;
+	}
+}
+
+
+void fill_arr_linear_downwards(key_t * arr, size_t n, key_t min, key_t max, int r) {
+	if (!arr) return;
+
+	key_t delta = (max - min);
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = delta * ((double)(n - 1 - i) / (n-1)) + min;
+	}
+}
+
+
+void fill_arr_sin(key_t* arr, size_t n, key_t min, key_t max, int r) {
+	if (!arr) return;
+	if (r == 0) return;
+
+	double delta = max - min;
+	double amplitude = delta / 2.0;
+
+	for (int i = 0; i < n; ++i) {
+		arr[i] = cos((i * M_PI * 2) / r) * amplitude + min + amplitude;
+	}
+}
+
+
+void fill_arr_sawtooth(key_t* arr, size_t n, key_t min, key_t max, int r) {
+	if (!arr) return;
+	if (r == 0) return;
+
+	double delta = max - min;
+
+	for (int i = 0; i < n; ++i) {
+		arr[i] = ((double)(i % (r+1)) / r) * delta + min;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 
 int comp_greater(const void* left, const void* right) {
     s_compCount++;
@@ -86,6 +152,7 @@ void swap(void* a, void* b, size_t size) {
     }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
 
 void sort_arr_bubble (void* arr, size_t n, size_t size, cmpFun_t compareFunction) {
     if (!compareFunction) compareFunction = comp_greater;
@@ -99,6 +166,7 @@ void sort_arr_bubble (void* arr, size_t n, size_t size, cmpFun_t compareFunction
         }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
 
 void sort_arr_bubble_m(void* arr, size_t n, size_t size, cmpFun_t compareFunction) {
     
@@ -116,9 +184,12 @@ void sort_arr_bubble_m(void* arr, size_t n, size_t size, cmpFun_t compareFunctio
     }
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+
 static int _quick_sort_partition(void* arr, int low, int high, size_t size, cmpFun_t compareFunction) {
     uint8_t* barr = arr;
-    uint8_t* value = barr + (low + (int)arr % (high-low)) * size;
+    // uint8_t* value = barr + (low + (int)arr % (high-low)) * size;
+    uint8_t* value = barr + ((high + low) / 2) * size;
 
     int i = low - 1, j = high;
     for (;;)
@@ -134,6 +205,7 @@ static int _quick_sort_partition(void* arr, int low, int high, size_t size, cmpF
     return i;
 }
 
+
 static void _quick_sort_range(void* arr, int low, int high, size_t size, cmpFun_t compareFunction) {
     int partitionIndex;
     if (high - low <= 0)
@@ -144,9 +216,12 @@ static void _quick_sort_range(void* arr, int low, int high, size_t size, cmpFun_
     _quick_sort_range(arr, partitionIndex + 1, high, size, compareFunction);
 }
 
+
 void sort_arr_quick(void* arr, size_t n, size_t size, cmpFun_t compareFunction) {
     _quick_sort_range(arr, 0, n-1, size, compareFunction);
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 void sort_arr_insertion(void* arr, size_t n, size_t size, cmpFun_t compareFunction) {
     int i, j;
@@ -163,6 +238,8 @@ void sort_arr_insertion(void* arr, size_t n, size_t size, cmpFun_t compareFuncti
     free(temp);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+
 static void _quick_sort_modified_range(void* arr, int low, int high, size_t size, cmpFun_t compareFunction) {
     int partitionIndex;
     if (high - low <= 0)
@@ -178,55 +255,13 @@ static void _quick_sort_modified_range(void* arr, int low, int high, size_t size
     _quick_sort_modified_range(arr, partitionIndex + 1, high, size, compareFunction);
 }
 
+
 void sort_arr_quick_m(void* arr, size_t n, size_t size, cmpFun_t compareFunction) {
     _quick_sort_modified_range(arr, 0, n-1, size, compareFunction);
 }
 
-
-void print_arr(const key_t* arr, size_t n) {
-    for (int i = 0; i < n; i++) printf("%"MODIFIER" ", arr[i]);
-    printf("\n");
-}
-
-
-void test_sort_perfomance(const key_t* srcarr, size_t n, sortFuncCell_t cellsort, 
-                          uint32_t* outTimedelta, uint64_t* outCompdelta
-) {
-    key_t* arr = malloc(sizeof (key_t) * n);
-    memcpy(arr, srcarr, sizeof(key_t) * n);
-    
-    DWORD t1 = get_tick_count();
-    reset_comp_count();
-
-    cellsort.invoke(arr, n, sizeof(key_t), comp_greater);
-
-    *outCompdelta = get_comp_count();
-    *outTimedelta = get_tick_count() - t1;
-
-    free(arr);
-}
-
-
-void printf_ex(FILE* file, const char* fmt, ...) {
-    va_list ap;
-
-    va_start(ap, fmt);
-    vprintf(fmt, ap);
-    va_end(ap);
-
-    va_start(ap, fmt);
-    vfprintf(file, fmt, ap);
-    va_end(ap);
-}
-
-
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
-
-
-// ?
-// Для каждого алгоритма сортировки гененрировать различные последовательности, 
-// или тест должен проводиться на одной созданной?
 
 #define N 8
 int main() {
@@ -278,6 +313,7 @@ int main() {
                      n += fsort.rangeStep
             ) {
                 key_t* arr = malloc(sizeof (key_t) * n);
+
                 ffill.invoke(arr, n, 0, n, 9);
 
                 DWORD delta = 0;
@@ -287,11 +323,11 @@ int main() {
                     reset_comp_count();
                     fsort.invoke(arr, n, sizeof(key_t), comp_greater);
 
-                    delta += get_tick_count() - t1;
+                    delta  += get_tick_count() - t1;
                     counts += get_comp_count();
                 }
 
-                delta /= INVOKES_COUNT;
+                delta  /= INVOKES_COUNT;
                 counts /= INVOKES_COUNT;
 
                 printf_ex(out, "%d;%d; ;%d;%d;\n", n, delta, n, counts);
@@ -304,107 +340,9 @@ int main() {
         printf_ex(out, "\n");
     }
 
-
     printf("[LOG] Done");
 
 	return 0;
 }
 
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-
-int random_int(int min, int max) {
-	return rand() % (max - min) + min;
-}
-
-
-double random_double(double min, double max) {
-	return ((double)rand() / RAND_MAX) * (max - min) + min;
-}
-
-
-void fill_arr_random(key_t * arr, size_t n, key_t min, key_t max, int r) {
-	if (!arr) return;
-
-	for (int i = 0; i < n; ++i) {
-		arr[i] = (key_t)random_double(min, max); 
-	}
-}
-
-
-void fill_arr_linear_upwards(key_t * arr, size_t n, key_t min, key_t max, int r) {
-	if (!arr) return;
-
-	key_t delta = (max - min);
-
-	for (int i = 0; i < n; ++i) {
-		arr[i] = delta * (i / (double)(n-1)) + min;
-	}
-}
-
-
-void fill_arr_linear_downwards(key_t * arr, size_t n, key_t min, key_t max, int r) {
-	if (!arr) return;
-
-	key_t delta = (max - min);
-
-	for (int i = 0; i < n; i++) {
-		arr[i] = delta * ((double)(n - 1 - i) / (n-1)) + min;
-	}
-}
-
-
-void fill_arr_sin(key_t* arr, size_t n, key_t min, key_t max, int r) {
-	if (!arr) return;
-	if (r == 0) return;
-
-	double delta = max - min;
-	double amplitude = delta / 2.0;
-
-	for (int i = 0; i < n; ++i) {
-		arr[i] = cos((i * M_PI * 2) / r) * amplitude + min + amplitude;
-	}
-}
-
-
-// https://studfile.net/preview/9690421/page:5/
-void fill_arr_sawtooth(key_t* arr, size_t n, key_t min, key_t max, int r) {
-	if (!arr) return;
-	if (r == 0) return;
-
-	double delta = max - min;
-
-	for (int i = 0; i < n; ++i) {
-		arr[i] = ((double)(i % (r+1)) / r) * delta + min;
-	}
-}
-
-
-void fill_arr_stepped(key_t* arr, size_t n, key_t min, key_t max, int r) {
-	if (!arr) return;
-	if (r == 0) return;
-
-	double delta = max - min;
-	double period = n / r;
-
-	for (int i = 0; i < n; ++i) {
-		arr[i] = delta * ((int)( (i / (double)(n-1)) * period) / period) + 
-				 random_double(0.0, period / 4)
-				 + min;
-	}
-}
-
-
-void fill_arr_quasi_ordered(key_t* arr, size_t n, key_t min, key_t max, int r) {
-	if (!arr) return;
-
-	key_t delta = (max - min);
-
-	for (int i = 0; i < n; ++i) {
-		arr[i] = delta * (i / (double)(n-1) + random_double(0.0, 0.4) - 0.2 ) + min;
-	}
-}
-
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
 
